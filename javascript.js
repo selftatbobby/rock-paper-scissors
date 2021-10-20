33962f37e0d04d5424c0e96e03137a6a5abe6119
Bobby Tat <messaroundacc1@hotmail.com>
2021-10-20 14:07:59 -0700

    created javascript.js and linked it to index.html

---
+++ javascript.js
@@ -0,0 +1,5 @@
+console.log('hello world')
+//computer will randomly return either rock, paper, or scissors
+//play single round of RPS with player's selection vs. computer's selection
+//declare winner of that single round with a message
+
